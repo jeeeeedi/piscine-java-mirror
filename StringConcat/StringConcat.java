b2e909b895256f04b6fa44dea4328d4f50ca2cf7
jreston <jedi.reston@gritlab.ax>
2025-08-11 17:11:04 +0300

    else ifs

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,8 +2,11 @@ public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1 != null && s2 != null) {
             return s1.concat(s2);
-        } else {
-            return null;
+        } else if (s1 != null && s2 == null) {
+            return s1;
+        } else if (s1 == null && s2 != null) {
+            return s2;
         }
+        return null;
     }
 }
