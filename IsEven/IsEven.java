7a19f6f329f092497672ce7641fa0c5dfad226ec
jreston <jedi.reston@gritlab.ax>
2025-08-11 16:53:44 +0300

    IsEven

---
+++ IsEven/IsEven.java
@@ -0,0 +1,9 @@
+public class IsEven {
+    public static boolean isEven(int a) {
+        if (a % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
