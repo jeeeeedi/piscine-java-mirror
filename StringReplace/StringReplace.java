da531658e16a4d31223c68033471e3fb698fdf94
jreston <jedi.reston@gritlab.ax>
2025-08-11 17:32:16 +0300

    Overloading is the standard and only way to support both signatures in Java.

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -5,4 +5,8 @@ public class StringReplace {
         }
         return s.replace(target, replacement);
     }
+
+    public static String replace(String s, char target, char replacement) {
+        return replace(s, String.valueOf(target), String.valueOf(replacement));
+    }
 }
