db8777d60e90ff2afdd2fba2bf70f87509e2a70d
jreston <jedi.reston@gritlab.ax>
2025-08-11 16:51:09 +0300

    FloatOperations

---
+++ FloatOperations/FloatOperations.java
@@ -0,0 +1,11 @@
+public class FloatOperations {
+    public static float addTwoFloats(float a, float b) {
+        return a + b;
+    }
+    public static float divideTwoFloats(float a, float b) {
+        if (b != 0) {
+            return a / b;
+        }
+        return 0;
+    }
+}
