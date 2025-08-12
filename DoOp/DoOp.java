4f6c7fdbe45cfbc4be8cdf4dbbad7d934bcd9b25
jreston <jedi.reston@gritlab.ax>
2025-08-12 16:38:05 +0300

    modulo

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -25,6 +25,9 @@ public class DoOp {
             case "/":
                 result = leftOperand / rightOperand;
                 break;
+            case "%":
+                result = leftOperand % rightOperand;
+                break;
             default:
                 return "Error";
         }
