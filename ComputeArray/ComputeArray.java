c2e4dcd4dbe69da5d66120584907cb0e69643e4e
jreston <jedi.reston@gritlab.ax>
2025-08-12 12:46:28 +0300

    else return same

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,16 +1,16 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
         if (array == null || array.length == 0) {
-            return new int[0];
+            return null;
         }
         int[] result = new int[array.length];
         for (int i = 0; i < array.length; i++) {
             int mod = array[i] % 3;
             if (mod == 0) {
                 result[i] = array[i] * 5;
-            } else if (mod == 1 || mod == -2) {
+            } else if (mod == 1) {
                 result[i] = array[i] + 7;
-            } else if (mod == 2 || mod == -1) {
+            } else {
                 result[i] = array[i];
             }
         }
