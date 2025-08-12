f8079c4634d48844cd5c26bf481af3765de87929
jreston <jedi.reston@gritlab.ax>
2025-08-12 12:53:35 +0300

    shouldReturnEmptyArray_whenArrayIsEmpty(), shouldReturnNull_whenNull()

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -1,19 +1,25 @@
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
-        if (array == null || array.length == 0) {
+        if (array == null) {
             return null;
         }
         int[] result = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            int mod = array[i] % 3;
-            if (mod == 0) {
-                result[i] = array[i] * 5;
-            } else if (mod == 1) {
-                result[i] = array[i] + 7;
-            } else {
-                result[i] = array[i];
+
+        if (array.length == 0) {
+            return new int[0];
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                int mod = array[i] % 3;
+                if (mod == 0) {
+                    result[i] = array[i] * 5;
+                } else if (mod == 1) {
+                    result[i] = array[i] + 7;
+                } else {
+                    result[i] = array[i];
+                }
             }
         }
         return result;
+
     }
 }
