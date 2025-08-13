f83a4a927d3f9036c06497425905e34b3e5692eb
jreston <jedi.reston@gritlab.ax>
2025-08-13 16:09:54 +0300

    StarOverride

--- StarInheritance/Star.java
+++ StarInheritance/Star.java
@@ -9,4 +9,26 @@ public class Star extends CelestialObject {
         this.magnitude = magnitude;
     }
 
+    public Star(String name, double x, double y, double z, double magnitude) {
+        super(name, x, y, z);
+        this.magnitude = magnitude;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(magnitude);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof Star)) return false;
+        Star other = (Star) object;
+        return Double.compare(magnitude, other.magnitude) == 0;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + " shines at the " + magnitude + " magnitude";
+    }
 }
