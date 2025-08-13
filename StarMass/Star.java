50898425936f925f988527f62b39b55bc9947d81
jreston <jedi.reston@gritlab.ax>
2025-08-13 17:03:45 +0300

    moved constructor

--- StarMass/Star.java
+++ StarMass/Star.java
@@ -7,16 +7,16 @@ public class Star extends CelestialObject {
         this.magnitude = 0.0;
     }
 
+    public Star(String name, double x, double y, double z, int mass, double magnitude) {
+        super(name, x, y, z, mass);
+        this.magnitude = magnitude;
+    }
+
     public double getMagnitude() {
         return magnitude;
     }
 
     public void setMagnitude(double magnitude) {
-        this.magnitude = magnitude;
-    }
-
-    public Star(String name, double x, double y, double z, int mass, double magnitude) {
-        super(name, x, y, z, mass);
         this.magnitude = magnitude;
     }
 
