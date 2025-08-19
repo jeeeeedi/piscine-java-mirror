ca2ff295715f5664f2745baa9a454734501ecaf0
jreston <jedi.reston@gritlab.ax>
2025-08-19 15:54:29 +0300

    AdventureUtils

--- AdventureCharacter/Character.java
+++ AdventureCharacter/Character.java
@@ -1,5 +1,5 @@
-/* import java.util.ArrayList;
-import java.util.List; */
+import java.util.ArrayList;
+import java.util.List;
 
 public class Character {
 
@@ -7,14 +7,13 @@ public class Character {
     private int currentHealth;
     // public int currentHealth; // AdventureSorcerer
     private final String name;
-    // private static List<Character> allCharacters = new ArrayList<>(); //
-    // AdventureUtils
+    private static List<Character> allCharacters = new ArrayList<>(); // AdventureUtils
 
     public Character(String name, int maxHealth) {
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
         this.name = name;
-        // allCharacters.add(this); // AdventureUtils
+        allCharacters.add(this); // AdventureUtils
     }
 
     public int getMaxHealth() {
@@ -54,36 +53,36 @@ public class Character {
     }
 
     /*--- AdventureUtils ---*/
-    /*
-     * public static String printStatus() {
-     * if (allCharacters.size() <= 0) {
-     * return "------------------------------------------\n" +
-     * "Nobody's fighting right now !\n" +
-     * "------------------------------------------";
-     * } else {
-     * StringBuilder status = new StringBuilder();
-     * status.append("------------------------------------------\n");
-     * status.append("Characters currently fighting : \n");
-     * for (Character character : allCharacters) {
-     * status.append(" - ").append(character.toString()).append("\n");
-     * }
-     * status.append("------------------------------------------");
-     * return status.toString();
-     * }
-     * }
-     * 
-     * public static Character fight(Character char1, Character char2) {
-     * while (char1.getCurrentHealth() > 0 || char2.getCurrentHealth() > 0) {
-     * char1.attack(char2);
-     * if (char2.getCurrentHealth() <= 0) {
-     * return char1;
-     * }
-     * char2.attack(char1);
-     * if (char1.getCurrentHealth() <= 0) {
-     * return char2;
-     * }
-     * }
-     * return null;
-     * }
-     */
+
+    public static String printStatus() {
+        if (allCharacters.size() <= 0) {
+            return "------------------------------------------\n" +
+                    "Nobody's fighting right now !\n" +
+                    "------------------------------------------";
+        } else {
+            StringBuilder status = new StringBuilder();
+            status.append("------------------------------------------\n");
+            status.append("Characters currently fighting : \n");
+            for (Character character : allCharacters) {
+                status.append(" - ").append(character.toString()).append("\n");
+            }
+            status.append("------------------------------------------");
+            return status.toString();
+        }
+    }
+
+    public static Character fight(Character char1, Character char2) {
+        while (char1.getCurrentHealth() > 0 || char2.getCurrentHealth() > 0) {
+            char1.attack(char2);
+            if (char2.getCurrentHealth() <= 0) {
+                return char1;
+            }
+            char2.attack(char1);
+            if (char1.getCurrentHealth() <= 0) {
+                return char2;
+            }
+        }
+        return null;
+    }
+
 }
