d712e0a5fa1c125fb35c554ade0a5ae2fb847332
jreston <jedi.reston@gritlab.ax>
2025-08-19 19:16:22 +0300

    attack method should be public

--- AdventureAbstract/Character.java
+++ AdventureAbstract/Character.java
@@ -55,14 +55,14 @@ public abstract class Character {
         }
     } */
 
-    abstract void attack(Character target); /* {
+    public void attack(Character target) {
         if (currentHealth > 0) {
             target.takeDamage(9);
             if (currentHealth < 0) {
                 currentHealth = 0;
             }
         }
-    } */
+    }
 
     /*--- AdventureUtils ---*/
 
