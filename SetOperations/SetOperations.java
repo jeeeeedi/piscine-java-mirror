4e2963a71c300fea3d73c63563fa3b3411d6d102
jreston <jedi.reston@gritlab.ax>
2025-08-23 10:05:06 +0300

    SetOperations

---
+++ SetOperations/SetOperations.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>();
+        if (set1 != null) result.addAll(set1);
+        if (set2 != null) result.addAll(set2);
+        return result;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>();
+        if (set1 == null || set2 == null) return result;
+        for (Integer elem : set1) {
+            if (set2.contains(elem)) {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
+}
